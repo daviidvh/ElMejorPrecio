da5834ce0088971bd71525ae68b8598091e71bfe
daviidvh <davvel833@alu.centroafuera.es>
2023-03-16 12:50:15 +0100

    Initial commit

---
+++ app/src/main/java/com/example/elmejorprecio/loggin.kt
@@ -0,0 +1,11 @@
+package com.example.elmejorprecio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class loggin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loggin)
+    }
+}
