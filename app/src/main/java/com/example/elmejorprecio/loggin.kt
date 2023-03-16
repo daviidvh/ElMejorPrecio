1c17e6c589ed2af963aa374e595b1bcd7a6e2847
daviidvh <davvel833@alu.centroafuera.es>
2023-03-16 18:29:12 +0100

    Interfaz Login mitad 16/03/2023

--- app/src/main/java/com/example/elmejorprecio/loggin.kt
+++ app/src/main/java/com/example/elmejorprecio/loggin.kt
@@ -1,11 +1,13 @@
-package com.example.elmejorprecio
+package com.examp0le.elmejorprecio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.elmejorprecio.databinding.ActivityLogginBinding
 
 class loggin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loggin)
+        val binding = ActivityLogginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
